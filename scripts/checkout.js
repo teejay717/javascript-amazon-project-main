cc288eaa5e638de0d2b29c33cb92dcf676ed4b6f
TJDantes <terenzdantes717@gmail.com>
2025-11-21 21:58:18 +0800

    Save cart in localStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,8 +17,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML += 
     `
-    <div class="cart-item-container 
-    js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -103,7 +102,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(`.js-cart-item-container-${matchingProduct.id}`);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
         container.remove();
     })
