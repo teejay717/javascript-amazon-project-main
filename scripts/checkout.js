6b787ae532f526f2488d1bf897226ced4ac62133
TJDantes <terenzdantes717@gmail.com>
2025-12-28 16:18:40 +0800

    Update delivery date display in cart summary based on selected delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -27,11 +27,26 @@ cart.forEach((cartItem) => {
         }
     })
 
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
+
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+
+    const dateString = deliveryDate.format('dddd, MMMM D');
+
     cartSummaryHTML += 
     `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
-            Delivery date: Tuesday, June 21
+            Delivery date: ${dateString}
         </div>
 
         <div class="cart-item-details-grid">
